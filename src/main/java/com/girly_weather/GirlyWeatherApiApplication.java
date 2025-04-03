73012c80c874db52b66c8b716916f1a5a9be1a96
Nailyn DFl <nailyn.diazf@gmail.com>
2025-04-02 21:38:59 -0500

    🎉 Added Spring Boot Weather App project.

---
+++ src/main/java/com/girly_weather/GirlyWeatherApiApplication.java
@@ -0,0 +1,12 @@
+package com.girly_weather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GirlyWeatherApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GirlyWeatherApiApplication.class, args);
+    }
+}
---
+++ src/test/java/com/girly_weather/GirlyWeatherApiApplicationTests.java
@@ -0,0 +1,11 @@
+package com.girly_weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GirlyWeatherApiApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
